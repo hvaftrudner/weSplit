be673e0e86cdae7ee1c632a02a51bcd1a29e52a7
hvaftrudner <krill_eriksson@hotmail.com>
2021-01-16 19:11:39 +0100

    Initial Commit

---
+++ weSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weSplit
+//
+//  Created by Kristoffer Eriksson on 2021-01-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
